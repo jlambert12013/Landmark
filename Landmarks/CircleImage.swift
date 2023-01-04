ef56fdc0ea8fca774a4db01f03fa38c79b1830ea
Jim Lambert <jlambert12013@gmail.om>
2023-01-03 20:05:21 -0600

    MapKit in Swift UI. Also, Adding thing into the ContentView and using Dividers.

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -14,6 +14,7 @@ struct CircleImage: View {
             .overlay {
                 Circle().stroke(.gray, lineWidth: 4)
             }
+            .shadow(radius: 7)
     }
 }
 
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,19 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Jim Lambert National Tree Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("Alabama")
-                    .font(.subheadline)
-            }
+        
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
             
+            CircleImage()
+                .offset(y: -130).padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Jim Lambert National Tree Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Alabama")
+                        .font(.subheadline)
+                }.font(.subheadline).foregroundColor(.secondary)
+                
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
+            }.padding()
+            
+            
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
